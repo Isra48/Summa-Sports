acedae606a6b0f6d793b775010bbdb1e85e237bf
Isra48 <israelalbarran48@gmail.com>
2021-10-10 00:04:58 -0500

    added section2, updated copies and images

--- src/pages/Home1.js
+++ src/pages/Home1.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Home1.css'
+import Image from '../img/section2.jpg'
+import Bg from '../img/Bg.jpg'
+
+
+
 
 export default function Home1() {
     return (
-        <div className="hero"> 
+        <div className="home">
+
+
+            <div className="hero"> 
                 <div className="header-content">
                     <div className="copies">
                     <h1 className="title">DEPORTISTAS HACIENDO <span>MARKETING PARA DEPORTISTAS </span></h1>
@@ -11,12 +19,63 @@ export default function Home1() {
                     </div>
 
                     <a className="btn"> hola putos </a>
-                
-            
-               
-                    
                 </div> 
+             </div>
+
+
+
+             <div className="section2"> 
+                <div className="father-box">
+                    <div className="item1">
+                        
+                        <img src={Image}/>
+
+                      </div>
+
+                      <div className="item2">
+                          <h2>"Pensamos por ti"</h2>
+                          
+                        <p>En SUMMA  hablamos el mismo idioma que tú. Somos un equipo conformado por deportistas de alto rendimiento y profesionistas de marketing con gran experiencia en medios deportivos, entendemos cómo funcionan los deportes y lo digital en esta era. 
+                        </p>
+
+                    </div>
+
+                </div> 
+            </div>
+
+
+            <div className="section2"> 
+                <div className="father-box">
+                    <div className="item1">
+                        
+                        <img src={Image}/>
+
+                      </div>
+
+                      <div className="item2">
+                          <h2>"Pensamos por ti"</h2>
+                          
+                        <p>En SUMMA  hablamos el mismo idioma que tú. Somos un equipo conformado por deportistas de alto rendimiento y profesionistas de marketing con gran experiencia en medios deportivos, entendemos cómo funcionan los deportes y lo digital en esta era. 
+                        </p>
+
+                    </div>
+
+                </div> 
+            </div>
+
+
+
+
         </div>
+
+        
+
+
+        
+        
+
+        
+
 
         
     )
