238d6fc58205ae50b5c6f39ab5631bcf08de6fba
jerrypkm <gerardochapa1000@hotmail.com>
2021-11-03 10:43:32 -0600

    Home responsive

--- src/pages/Home1.js
+++ src/pages/Home1.js
@@ -3,132 +3,101 @@ import './Home1.css'
 import Image from '../img/section2.jpg'
 import Patern from '../img/Patern.jpg'
 
-
-
-
 export default function Home1() {
-    return (
-        <div className="home">
+  return (
+    <div className="home">
+      <div className="hero">
+        <div className="header-content">
+          <div className="copies">
+            <h1 className="title">
+              DEPORTISTAS HACIENDO <span>MARKETING PARA DEPORTISTAS </span>
+            </h1>
+          </div>
 
+          <a className="btn"> hola putos </a>
+        </div>
+      </div>
 
-            <div className="hero"> 
-                <div className="header-content">
-                    <div className="copies">
-                    <h1 className="title">DEPORTISTAS HACIENDO <span>MARKETING PARA DEPORTISTAS </span></h1>
-                        
-                    </div>
+      <div className="section2">
+        <div className="father-box">
+          <div className="item1">
+            <img src={Image} />
+          </div>
 
-                    <a className="btn"> hola putos </a>
-                </div> 
-             </div>
+          <div className="item2">
+            <h2 className="titlei2">"Pensamos por ti"</h2>
 
+            <p>
+              En SUMMA hablamos el mismo idioma que tú. Somos un equipo
+              conformado por deportistas de alto rendimiento y profesionistas de
+              marketing con gran experiencia en medios deportivos, entendemos
+              cómo funcionan los deportes y lo digital en esta era.
+            </p>
+          </div>
+        </div>
+      </div>
 
-
-             <div className="section2"> 
-                <div className="father-box">
-                    <div className="item1">
-                        
-                        <img src={Image}/>
-
-                      </div>
-
-                      <div className="item2">
-                          <h2 className="titlei2">"Pensamos por ti"</h2>
-                          
-                        <p>En SUMMA  hablamos el mismo idioma que tú. Somos un equipo conformado por deportistas de alto rendimiento y profesionistas de marketing con gran experiencia en medios deportivos, entendemos cómo funcionan los deportes y lo digital en esta era. 
-                        </p>
-
-                    </div>
-
-                </div> 
+      <div className="section3">
+        <h1 className="titles3">SERVICIOS</h1>
+        <div class="services__wrapper">
+          <div class="services__card">
+            <h2>Social Media</h2>
+            <p>
+              La interacción que tienes en tus redes es proporcional a la
+              cantidad de posibilidad de nuevos clientes.
+            </p>
+            <div class="services__btn">
+              <button>Get Started</button>
             </div>
+          </div>
 
-
-            <div className="section3"> 
-            <h1 className="titles3">SERVICIOS</h1>
-            <div class="services__wrapper">
-
-                <div class="services__card">
-                    <h2>Social Media</h2>
-                     <p>La interacción que tienes en tus redes es proporcional a la cantidad de posibilidad de nuevos clientes.</p>
-                <div class="services__btn"><button>Get Started</button></div>
-                </div>
-
-                <div class="services__card">
-                    <h2>Web Development</h2>
-                     <p>Tu carta de presentación en esta era digital.</p>
-                <div class="services__btn"><button>Get Started</button></div>
-                </div>
-
-                <div class="services__card">
-                    <h2>E-commerce</h2>
-                     <p>La cercania con tus clientes 24/7</p>
-                <div class="services__btn"><button>Get Started</button></div>
-                </div>
-                <div class="services__card">
-                    <h2>Events planning</h2>
-                     <p>¿Quieres marcar tendencia en el mundo de los eventos? Estás con el equipo correcto.</p>
-                <div class="services__btn"><button>Get Started</button></div>
-                </div>
-
+          <div class="services__card">
+            <h2>Web Development</h2>
+            <p>Tu carta de presentación en esta era digital.</p>
+            <div class="services__btn">
+              <button>Get Started</button>
             </div>
+          </div>
 
-
-
-
-
-
-            <div className="section2"> 
-                <div className="father-box">
-                    <div className="item1">
-                        
-                        <img src={Patern} className="patern"/>
-
-                      </div>
-
-                      <div className="item2">
-                          <h2 className="title4">Nosotros</h2>
-                          
-                        <p>Es hora de llevar tu negocio a otro nivel posicionándolo digitalmente. Posicionar tu marca ya no es más un trabajo para novatos, es un trabajo que requiere de profesionales. Deja tu negocio en manos de nuestros expertos y permítenos sumar valor a tu negocio a un precio ¡irresistible!
-                        </p>
-
-                    </div>
-
-                </div> 
+          <div class="services__card">
+            <h2>E-commerce</h2>
+            <p>La cercania con tus clientes 24/7</p>
+            <div class="services__btn">
+              <button>Get Started</button>
             </div>
-
-            
-           
-       
-      
-        
-
-            
-
-            
-
-
-
-
-
-
-              
-                   
+          </div>
+          <div class="services__card">
+            <h2>Events planning</h2>
+            <p>
+              ¿Quieres marcar tendencia en el mundo de los eventos? Estás con el
+              equipo correcto.
+            </p>
+            <div class="services__btn">
+              <button>Get Started</button>
             </div>
-
-
+          </div>
         </div>
 
-        
+        <div className="section2">
+          <div className="father-box">
+            <div className="item1">
+              <img src={Patern} className="patern" />
+            </div>
 
+            <div className="item2">
+              <h2 className="title4">Nosotros</h2>
 
-        
-        
-
-        
-
-
-        
-    )
+              <p>
+                Es hora de llevar tu negocio a otro nivel posicionándolo
+                digitalmente. Posicionar tu marca ya no es más un trabajo para
+                novatos, es un trabajo que requiere de profesionales. Deja tu
+                negocio en manos de nuestros expertos y permítenos sumar valor a
+                tu negocio a un precio ¡irresistible!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
-
